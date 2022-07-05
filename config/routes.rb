e0ffb31a573fc0098e9dca59be0f8272aa5e1503
EC2 Default User <ec2-user@ip-172-31-19-219.ec2.internal>
2022-07-05 07:01:42 +0000

    tyuubaxn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-root to: "homes#top"
-devise_for :users
+  devise_for :users
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
 
-resources :books, only: [:index, :show]
-get "/homes/about" => "homes#about", as: "about"
+  resources :books, only: [:index, :show]
+  resources :users, only: [:show, :edit]
 end
