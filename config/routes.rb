ebeef63c52a1a208aa89be6c0e89562ad3609872
EC2 Default User <ec2-user@ip-172-31-19-219.ec2.internal>
2022-07-09 06:55:02 +0000

    11章

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
 
-  resources :books, only: [:index, :show]
+  resources :books, only: [:new, :create, :index, :show]
   resources :users, only: [:show, :edit]
 end
