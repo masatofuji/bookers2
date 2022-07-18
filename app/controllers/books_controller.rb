a3d093b6dc804b120c4489a48f3e7b7347688d19
EC2 Default User <ec2-user@ip-172-31-19-219.ec2.internal>
2022-07-18 04:38:24 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-  before_action :correct_user, only: [:edit, :update]
+  before_action :ensure_correct_user, only:[:edit]
+
 
   def index
     @book = Book.new
@@ -21,8 +22,16 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.new
+    @books = Book.find(params[:id])
+    @user = @books.user
+  end
+
+  def ensure_correct_user
     @book = Book.find(params[:id])
-    @user = current_user
+    unless @book.user ==current_user
+    redirect_to books_path
+    end
   end
 
   def edit
@@ -52,9 +61,4 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
-  def correct_user
-    @book = Book.find(params[:id])
-    @user = @book.user
-    redirect_to(user_path) unless @user == current_user
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
 
   def index
+    @book = Book.new
     @user = current_user
     @users = User.all
   end
 
   def show
+    @book = Book.new
     @user = User.find(params[:id])
     @books = @user.books
   end
@@ -28,5 +31,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(user_path(current_user.id)) unless @user == current_user
   end
 end
